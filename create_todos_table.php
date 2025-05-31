1d052b754e592c357f94e314c3a79c317289fe5f
Manhar Hadiyal <hadiyalmd@gmail.com>
2025-05-31 11:48:06 +0530

    Create create_todos_table.php

---
+++ create_todos_table.php
@@ -0,0 +1,9 @@
+public function up()
+{
+    Schema::create('todos', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->boolean('completed')->default(false);
+        $table->timestamps();
+    });
+}
