5c547de1f78d39d92524c9a35163eadb4e1092dc
Manhar Hadiyal <manharwebdev@gmail.com>
2025-12-02 12:29:59 +0530

    Update create_todos_table.php

--- create_todos_table.php
+++ create_todos_table.php
@@ -7,3 +7,13 @@ public function up()
         $table->timestamps();
     });
 }
+
+public function up()
+{
+    Schema::create('todos', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->boolean('completed')->default(false);
+        $table->timestamps();
+    });
+}
